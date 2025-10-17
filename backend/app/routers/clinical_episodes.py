03c71233a752df61032876d8395a2447346b8824
Borja Marquez de la Plata <bmarquezdelaplata@uc.cl>
2025-10-17 12:42:30 -0300

    feat: added include enum to improve documentation

--- backend/app/routers/clinical_episodes.py
+++ backend/app/routers/clinical_episodes.py
@@ -5,20 +5,26 @@ from sqlalchemy import select
 from fastapi import Depends
 from app.deps import get_session
 from sqlalchemy.orm import selectinload
+from enum import Enum
 from typing import Union
 
 from app.models.clinical_episode import ClinicalEpisode as ClinicalEpisodeModel
 from app.schemas.clinical_episode import ClinicalEpisodeWithPatient, ClinicalEpisode
 
 
+
 router = APIRouter(prefix="/clinical-episodes", tags=["clinical-episodes"])
+
+
+class ClinicalEpisodeInclude(str, Enum):
+    PATIENT = "patient"
 
 @router.get("/", response_model=Union[list[ClinicalEpisodeWithPatient], list[ClinicalEpisode]])
 async def list_clinical_episodes(
-    include: str | None = None,
+    include: ClinicalEpisodeInclude | None = None,
     session: AsyncSession = Depends(get_session)
 ) -> Union[list[ClinicalEpisodeWithPatient], list[ClinicalEpisode]]:
-    if include == "patient":
+    if include == ClinicalEpisodeInclude.PATIENT:
         clinical_episodes = select(ClinicalEpisodeModel)
         clinical_episodes_with_patient_info = clinical_episodes.options(selectinload(ClinicalEpisodeModel.patient))
         result = await session.execute(clinical_episodes_with_patient_info)
