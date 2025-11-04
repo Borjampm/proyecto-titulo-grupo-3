8cc72dcc70f334bc63cef61ff76942b2b5909c97
Eduardo Esquivel <eaesquivel@uc.cl>
2025-11-04 11:00:51 -0300

    add CORS allow any origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <-- 1. IMPORT THIS
 
 from app.deps import lifespan
 from app.routers.patients import router as patients_router
@@ -8,6 +9,20 @@ from app.routers.excel_upload import router as excel_upload_router
 
 app = FastAPI(lifespan=lifespan)
 
+# Esto es para cambiar CORS. Eventualmente deberíamos cambiar
+# origins para que contenga sólo el dominio de nuestro (eventual)
+# frontend. Por ahora, simplemente haré que acepte cualquier origen.
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # Specifies the allowed origins
+    allow_credentials=True,    # Allows cookies/auth headers
+    allow_methods=["*"],       # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],       # Allows all headers
+)
 
 @app.get("/")
 async def read_root() -> dict[str, str]:
@@ -18,3 +33,4 @@ app.include_router(patients_router)
 app.include_router(clinical_episodes_router)
 app.include_router(task_instances_router)
 app.include_router(excel_upload_router)
+
